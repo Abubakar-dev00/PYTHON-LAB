0fff334ce6f6951ad954198cca2e5c48e33022eb
Abubakar-dev00 <betaditech@gmail.com>
2026-03-04 10:12:30 +0500

    initial commit with files gui.py, lab_4.py, login_page.py, practice 4.py,

--- practice 4.py
+++ practice 4.py
@@ -3,17 +3,13 @@ import tkinter as tk
 window=tk.Tk()
 window.geometry("500x500")
 
-def button_Chng():
-    button_clr=button.cget("bg")
-    if(button_clr=="red"):
-        button.config(bg="green")
-    else:
-        button.config(bg="red")
 
 
 
 
-button=tk.Button(window,text="Click me",bg="red", command=button_Chng)
+
+
+button=tk.Button(window,text="Click me",bg="red")
 button.pack()
 
 window.mainloop()
